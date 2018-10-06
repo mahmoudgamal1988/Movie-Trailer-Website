f5787ae303186abeb2a079af2377296079976431
mahmoudgamal1988 <40371141+mahmoudgamal1988@users.noreply.github.com>
2018-10-06 22:04:54 +0100

    Update media.py

--- media.py
+++ media.py
@@ -1,23 +1,10 @@
-#importing webbrowser module which allows to open web links.
-import webbrowser
-
 class Movie():
-	""""a calss which we use to provide movies' data """
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_rating_on_idmb, movie_category):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		self.rating_on_idmb = movie_rating_on_idmb
-		self.category = movie_category
-
-'''an instance method with the function show_trailer,
-which allows accessing the provided youtube links.'''
-def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
-
-
-
-
-
-
+    """"a calss which we use to provide movies' data """
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, movie_rating_on_idmb, movie_category):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.rating_on_idmb = movie_rating_on_idmb
+        self.category = movie_category
